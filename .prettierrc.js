533585a1aed47572927bbded97cc123ccb43e1cc
peer <peer@infi.nl>
2020-01-29 23:17:16 +0100

    couple apollo

---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  semi: false,
+  trailingComma: true,
+  singleQuote: true,
+}
--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,19 +1,58 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import gql from 'graphql-tag'
 import { useQuery, useMutation } from '@apollo/react-hooks'
 import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
+const ALL_PETS = gql`
+  query AllPets {
+    pets {
+      id
+      name
+      type
+      img
+    }
+  }
+`
 
-export default function Pets () {
+const CREATE_PET = gql`
+  mutation CreatePet($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      id
+      name
+      type
+      img
+    }
+  }
+`
+
+export default function Pets() {
   const [modal, setModal] = useState(false)
-
+  const { data, loading, error } = useQuery(ALL_PETS)
+  const [createPet, { mdata, mloading, merror }] = useMutation(CREATE_PET)
 
   const onSubmit = input => {
     setModal(false)
+    createPet({
+      variables: {
+        newPet: {
+          name: input.name,
+          type: input.type,
+        },
+      },
+    })
   }
-  
+
+  if (loading || mloading) {
+    return <Loader />
+  }
+
+  if (error || merror) {
+    const leError = error || merror
+    return <p>{leError.message}</p>
+  }
+
   if (modal) {
     return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
   }
@@ -32,7 +71,7 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets} />
       </section>
     </div>
   )
